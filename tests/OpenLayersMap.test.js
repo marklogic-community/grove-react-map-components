fdb09bf1191c5d75c74cfd9babf0a6fa97d31325
Mae Isabelle Turiana <maeisabelle@gmail.com>
2019-11-05 00:43:37 +0800

    Updated tests and readme

--- tests/OpenLayersMap.test.js
+++ tests/OpenLayersMap.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import expect from 'expect';
-import OpenLayersMap from 'src/OpenLayersMap';
+import OpenLayersMap from '../src/OpenLayersMap';
 
 describe('<OpenLayersMap />', () => {
     it('renders without crashing', () => {
--- tests/setupTests.js
+++ tests/setupTests.js
@@ -2,5 +2,5 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
-let context = require.context('../src', true, /\.test\.js$/);
+let context = require.context('../tests', true, /\.test\.js$/);
 context.keys().forEach(context);
